95064a1f17e24f8c5e15bbb05d509e7709a46390
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 22:56:47 +0100

    first change

--- flaskapp.py
+++ flaskapp.py
@@ -1,23 +1,24 @@
-import os
-from random   import random
-from datetime import datetime
-from flask    import Flask, request, flash, url_for, redirect, \
-                     render_template, abort, send_from_directory
+from composes.semantic_space.space import Space
+from composes.similarity.cos       import CosSimilarity
+from composes.utils                import io_utils
+from flask                         import Flask
+from random                        import choice
 
+
+dat = io_utils.load('data.pkl')
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/<path:resource>')
-def serveStaticResource(resource):
-    return send_from_directory('static/', resource)
-
-@app.route("/test")
-def test():
     return "<strong>It's Alive!</strong>"
+
+
+@app.route('/generate/secret')
+def generate_secret():
+    return choice(dat.id2row)
+
 
 if __name__ == '__main__':
     app.run()
