0ad885a39d8496e309dbd53690e522159fd4fe9e
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 17:56:30 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -6,5 +6,13 @@ setup(name='FlaskApp',
       author='Ryan Jarvinen',
       author_email='ryanj@redhat.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
-     )
+      install_requires=['Flask>=0.10.1'
+                       ,'future'
+                       ,'names'
+                       ,'enum'
+                       ,'numpy'
+                       ,'scipy'
+                       ,'cython'
+                       ,'sparsesvd'
+                       ,'dissect==0.1.0'
+                       ,'nltk>=3.2'])
