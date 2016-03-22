3d73c0636c6aa622a209ae32aa6c79df6f2b0872
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 23:38:28 +0100

    finished basic api

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,17 @@
+"""
+Created on Mar 22, 2016
+
+@author: Pepijn Kokke
+"""
 from composes.semantic_space.space import Space
 from composes.similarity.cos       import CosSimilarity
 from composes.utils                import io_utils
 from flask                         import Flask
+from json                          import dumps as json
 from random                        import choice
 from nltk.corpus                   import wordnet as wn
 from nltk.corpus                   import cmudict as cm
+
 
 sim = CosSimilarity()
 dat = io_utils.load('data.pkl')
@@ -14,24 +21,25 @@ app.config.from_pyfile('flaskapp.cfg')
 
 @app.route('/generate/secret')
 def generate_secret():
-    secret = choice(dat.id2row)
-    output = [secret]
-    for synset in wn.synsets(secret):
-        if synset.name().startswith(secret+'.n.'):
-            output.append("It's " + synset.definition() + ".")
-    return '<br />'.join(output)
+    return json(choice(dat.id2row))
+
+
+@app.route('/definitions/<this>')
+def definitions(this):
+    return json([s.definition() for s in wn.synsets(this)
+                  if s.name().startswith(this+'.n.')])
 
 
 @app.route('/similarity/<this>/<that>')
 def similarity(this, that):
-    return str(dat.get_sim(this, that, sim))
+    return json(dat.get_sim(this, that, sim))
 
 
 @app.route('/startswith/vowel/<this>')
 def startswith_vowel(this):
     for syllables in cm.dict().get(this,[]):
-        return str(syllables[0][-1].isdigit())
-    return str(False)
+        return json(syllables[0][-1].isdigit())
+    return json(False)
 
 
 @app.route('/test')
