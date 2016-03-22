cbb645a0c1e11067e54ccb734073c31d3a87e55e
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 23:01:08 +0100

    added wordnet dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from setuptools import setup
+from setuptools                 import setup, find_packages
+from setuptools.command.install import install as _install
+
+
+class Install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        import nltk
+        nltk.download('wordnet')
+
 
 setup(name='Breadbox',
-      version='1.0',
-      description='Implementation of "Breadbox" or "Plenty Questions".',
-      author='Pepijn Kokke',
-      author_email='pepijn.kokke@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.10.1','future','names','enum','numpy'
-                        ,'scipy','cython','sparsesvd','dissect','nltk'])
+      version          ='1.0',
+      description      ='Implementation of "Breadbox" or "Plenty Questions".',
+      author           ='Pepijn Kokke',
+      author_email     ='pepijn.kokke@gmail.com',
+      url              ='http://www.python.org/sigs/distutils-sig/',
+      cmdclass         ={'install': Install},
+      install_requires =['Flask>=0.10.1','future','names','enum','numpy'
+                        ,'scipy','cython','sparsesvd','dissect','nltk'],
+      setup_requires   =['nltk'])
