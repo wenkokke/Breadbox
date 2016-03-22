cd0d3fac105a1c31e615c8bab21e3aab71021f00
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 22:42:50 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -2,18 +2,9 @@ from setuptools import setup
 
 setup(name='Breadbox',
       version='1.0',
-      description='A basic Flask app with static files',
+      description='Implementation of "Breadbox" or "Plenty Questions".',
       author='Pepijn Kokke',
-      author_email='pepijnkokke@gmail.com',
+      author_email='pepijn.kokke@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=[
-          'Flask>=0.10.1'
-          ,'future'
-          ,'names'
-          ,'enum'
-          ,'numpy'
-          ,'scipy'
-          ,'cython'
-          ,'sparsesvd'
-          ,'dissect'
-          ,'nltk'])
+      install_requires=['Flask>=0.10.1','future','names','enum','numpy'
+                        ,'scipy','cython','sparsesvd','dissect','nltk'])
