5e5ae9a32cc98058a1bb9f4f78305239786ffbdc
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 18:01:23 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='Breadbox',
       version='1.0',
       description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      author='Pepijn Kokke',
+      author_email='pepijnkokke@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.10.1'
                        ,'future'
