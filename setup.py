58e85b9863ec73a8531cbbdcd921d2f05b14f8f6
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 22:34:25 +0100

    starting rewrite to flask

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='Breadbox',
+      version='1.0',
+      description='A basic Flask app with static files',
+      author='Pepijn Kokke',
+      author_email='pepijnkokke@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=[
+          'Flask>=0.10.1'
+          ,'future'
+          ,'names'
+          ,'enum'
+          ,'numpy'
+          ,'scipy'
+          ,'cython'
+          ,'sparsesvd'
+          ,'dissect'
+          ,'nltk'])
