f20448676600937ddd9564d6f490ec7f4b3bb56a
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-22 23:28:55 +0100

    added required functionality

--- flaskapp.py
+++ flaskapp.py
@@ -4,7 +4,9 @@ from composes.utils                import io_utils
 from flask                         import Flask
 from random                        import choice
 from nltk.corpus                   import wordnet as wn
+from nltk.corpus                   import cmudict as cm
 
+sim = CosSimilarity()
 dat = io_utils.load('data.pkl')
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
@@ -23,6 +25,18 @@ def generate_secret():
         if synset.name().startswith(secret+'.n.'):
             output.append("It's " + synset.definition() + ".")
     return '<br />'.join(output)
+
+
+@app.route('/similarity/<this>/<that>')
+def similarity(this, that):
+    return str(dat.get_sim(this, that, sim))
+
+
+@app.route('/startswith/vowel/<this>')
+def startswith_vowel(this):
+    for syllables in cm.dict().get(this,[]):
+        return str(syllables[0][-1].isdigit())
+    return str(False)
 
 
 if __name__ == '__main__':
