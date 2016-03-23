bcfcaa43b4f9368129e525b376d22d0e051c43cb
Wen Kokke <wenkokke@users.noreply.github.com>
2016-03-23 10:02:12 +0100

    working out initial skeleton of app

--- flaskapp.py
+++ flaskapp.py
@@ -6,13 +6,14 @@ Created on Mar 22, 2016
 from composes.semantic_space.space import Space
 from composes.similarity.cos       import CosSimilarity
 from composes.utils                import io_utils
-from flask                         import Flask, render_template
-from json                          import dumps as json
-from random                        import choice
+from flask                         import Flask, render_template, send_from_directory
+from inflect                       import engine  as ie
+from json                          import dumps   as json
 from nltk.corpus                   import wordnet as wn
-from nltk.corpus                   import cmudict as cm
+from random                        import choice
 
 
+inf = ie()
 sim = CosSimilarity()
 dat = io_utils.load('corpora/composes.pkl')
 app = Flask(__name__)
@@ -21,31 +22,28 @@ app.config.from_pyfile('flaskapp.cfg')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    secret = choice(dat.id2row)
+    return render_template('index.html', secret=secret)
 
+@app.route('/<path:resource>')
+def serveStaticResource(resource):
+    return send_from_directory('static/', resource)
 
 @app.route('/generate/secret')
 def generate_secret():
     return json(choice(dat.id2row))
-
 
 @app.route('/definitions/<this>')
 def definitions(this):
     return json([s.definition() for s in wn.synsets(this)
                   if s.name().startswith(this+'.n.')])
 
-
-@app.route('/similarity/<this>/<that>')
+@app.route('/guess/<this>/<that>')
 def similarity(this, that):
-    return json(dat.get_sim(this, that, sim))
-
-
-@app.route('/startswith/vowel/<this>')
-def startswith_vowel(this):
-    for syllables in cm.dict().get(this,[]):
-        return json(syllables[0][-1].isdigit())
-    return json(False)
-
+    return json({
+        'similarity' : dat.get_sim(this, that, sim),
+        'a_or_an'    : inf.a(that)
+        })
 
 @app.route('/test')
 def test():
