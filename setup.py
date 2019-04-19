359fba4278a82ad8233626c273bb8cdbebf11db8
Wen Kokke <wenkokke@users.noreply.github.com>
2019-04-19 22:29:22 +0200

    Rewrote webapp to automatically download data

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-from setuptools                 import setup, find_packages
-from setuptools.command.install import install as _install
-
-class Install(_install):
-    def run(self):
-        if not self.dry_run:
-            _install.do_egg_install(self)
-
-            # download 'wordnet'
-            import nltk
-            nltk.download('wordnet')
-
-            # create 'data' directory
-            import os
-            if not os.path.isdir('data'):
-                os.mkdir('data')
-
-            # download 'GoogleNews SLIM'
-            from urllib.request import urlopen
-            url = "https://github.com/eyaler/word2vec-slim/raw/master/GoogleNews-vectors-negative300-SLIM.bin.gz"
-            resp = urlopen(url)
-            CHUNK = 16 * 1024
-            with open('data/GoogleNews-vectors-negative300-SLIM.bin.gz','wb') as f:
-                while True:
-                    chunk = resp.read(CHUNK)
-                    if not chunk:
-                        break
-                    f.write(chunk)
+from setuptools import setup
 
 setup(name='Breadbox',
       version          ='2.0',
@@ -33,6 +6,5 @@ setup(name='Breadbox',
       author           ='Wen Kokke',
       author_email     ='wen.kokke@gmail.com',
       url              ='http://www.python.org/sigs/distutils-sig/',
-      cmdclass         ={'install': Install},
       install_requires =['flask>=1.0.2','gensim>=3.7.2','inflect>=2.1.0','nltk>=3.3'],
-      setup_requires   =['nltk'])
+)
--- wsgi.py
+++ wsgi.py
@@ -8,16 +8,33 @@ import flask
 import gensim
 import inflect
 import json
+import nltk
+import os
 import random
-import nltk.corpus
+import urllib.request
 
-allowed_origins = [
-    'http://127.0.0.1:5000',
-    'https://127.0.0.1:5000',
-    'http://wenkokke.github.io',
-    'https://wenkokke.github.io',
-]
+# download 'wordnet'
+nltk.download('wordnet')
 
+# download 'GoogleNews SLIM'
+data_file = 'GoogleNews-vectors-negative300-SLIM.bin.gz'
+data_url = "https://github.com/eyaler/word2vec-slim/raw/master/{}".format(data_file)
+if not os.path.isfile(data_file):
+    chunk_size = 16 * 1024
+    resp = urllib.request.urlopen(data_url)
+    with open(data_file, 'wb') as f:
+        while True:
+            chunk = resp.read(chunk_size)
+            if not chunk:
+                break
+            f.write(chunk)
+
+
+# set allowed origins
+allowed_origins = ['http://127.0.0.1:5000', 'https://127.0.0.1:5000',
+                   'http://wenkokke.github.io', 'https://wenkokke.github.io',]
+
+# load app and data
 app = flask.Flask(__name__)
 model = gensim.models.KeyedVectors.load_word2vec_format(
     'data/GoogleNews-vectors-negative300-SLIM.bin.gz', binary=True)
