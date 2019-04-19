ac20d82263e7540f37f2042d4994a6a06434075e
Wen Kokke <wenkokke@users.noreply.github.com>
2019-04-19 14:28:02 +0200

    Fixed breadbox webapp

---
+++ breadbox.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""
+@author: Wen Kokke
+"""
+
+import flask
+import gensim
+import inflect
+import json
+import random
+import nltk.corpus
+
+allowed_origins = [
+    'http://127.0.0.1:5000',
+    'https://127.0.0.1:5000',
+    'http://wenkokke.github.io',
+    'https://wenkokke.github.io',
+]
+
+app = flask.Flask(__name__)
+model = gensim.models.KeyedVectors.load_word2vec_format(
+    'data/GoogleNews-vectors-negative300-SLIM.bin.gz', binary=True)
+vocab = list(model.vocab.keys())
+inflect = inflect.engine()
+
+@app.route('/')
+def hello():
+    return flask.render_template('index.html',**new_secret())
+
+@app.route('/guess/<this>/<that>')
+def guess(this, that):
+    origin = flask.request.headers.get('origin')
+    if origin is None or origin in allowed_origins:
+        resp = flask.Response(json.dumps({
+            'similarity': str(model.wv.similarity(this, that)),
+            'a_or_an': inflect.a(that)
+        }))
+        resp.headers['Access-Control-Allow-Origin'] = origin
+        return resp
+    return ''
+
+@app.route('/generate/secret')
+def generate_secret():
+    origin = request.headers.get('origin')
+    if origin is None or origin in origins:
+        resp = Response(json(new_secret()))
+        resp.headers['Access-Control-Allow-Origin'] = origin
+        return resp
+    return ''
+
+@app.route('/<path:resource>')
+def serveStaticResource(resource):
+    return flask.send_from_directory('static/', resource)
+
+def new_secret():
+    secret = random.choice(vocab)
+    secret_with_article = inflect.a(secret)
+    synsets = nltk.corpus.wordnet.synsets(secret)
+    definitions = [s.definition() for s in synsets if s.name().startswith(secret+".n")]
+    return {
+        'secret': secret,
+        'secret_with_article': secret_with_article,
+        'definitions': definitions,
+    }
