6919bb2978ab8bcc5f4114a4c8b3766eb0e0ff93
Wen Kokke <wenkokke@users.noreply.github.com>
2019-04-20 12:45:58 +0200

    Moved NLTK install to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+class Install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        import nltk
+        nltk.download("wordnet")
 
 setup(name='Breadbox',
-      version          ='2.0',
-      description      ='Implementation of "Breadbox" or "Plenty Questions".',
-      author           ='Wen Kokke',
-      author_email     ='wen.kokke@gmail.com',
-      url              ='http://www.python.org/sigs/distutils-sig/',
-      install_requires =['flask>=1.0.2','gensim>=3.7.2','inflect>=2.1.0','nltk>=3.3'],
-)
+      version          = '2.0',
+      description      = 'Implementation of "Breadbox" or "Plenty Questions".',
+      author           = 'Wen Kokke',
+      author_email     = 'wen.kokke@gmail.com',
+      url              = 'http://www.python.org/sigs/distutils-sig/',
+      cmdclass         = {'install': Install},
+      install_requires = ['flask>=1.0.2','gensim>=3.7.2','inflect>=2.1.0','nltk>=3.3'],
+      setup_requires   = ['nltk'],)
